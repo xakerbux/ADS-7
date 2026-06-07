ed458e99e0b7189ac4ab3f20080661c7272af362
xakerbux <156354805+xakerbux@users.noreply.github.com>
2026-06-07 23:40:37 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,74 +1,24 @@
 // Copyright 2025 NNTU-CS
-#include "train.h"
+#ifndef INCLUDE_TRAIN_H_
+#define INCLUDE_TRAIN_H_
 
-Train::Train() : first(nullptr), countOp(0) {}
+class Train {
+ public:
+    Train();
+    ~Train();
+    void addCar(bool light);
+    int getLength();
+    int getOpCount();
 
-Train::~Train() {
-    if (!first) return;
-    Car* current = first;
-    do {
-        Car* next = current->next;
-        delete current;
-        current = next;
-    } while (current != first);
-}
+ private:
+    struct Car {
+        bool light;
+        Car* next;
+        Car* prev;
+        explicit Car(bool l) : light(l), next(nullptr), prev(nullptr) {}
+    };
+    Car* first;
+    int countOp;
+};
 
-void Train::addCar(bool light) {
-    Car* newCar = new Car(light);
-    if (!first) {
-        first = newCar;
-        newCar->next = newCar;
-        newCar->prev = newCar;
-    } else {
-        Car* last = first->prev;
-        last->next = newCar;
-        newCar->prev = last;
-        newCar->next = first;
-        first->prev = newCar;
-    }
-}
-
-int Train::getLength() {
-    if (!first) return 0;
-    countOp = 0;
-    Car* current = first;
-    int len = 1;
-    bool done = false;
-    
-    while (!done) {
-        // Идём вперёд на len шагов
-        for (int i = 0; i < len; ++i) {
-            current = current->next;
-            countOp++;
-        }
-        
-        if (current->light) {
-            // Включён -> выключаем и возвращаемся
-            current->light = false;
-            for (int i = 0; i < len; ++i) {
-                current = current->prev;
-                countOp++;
-            }
-            len++;
-        } else {
-            // Выключён -> включаем и идём назад до первого
-            current->light = true;
-            int steps = 0;
-            while (current != first) {
-                current = current->prev;
-                countOp++;
-                steps++;
-            }
-            if (steps == len) {
-                done = true;
-            } else {
-                len++;
-            }
-        }
-    }
-    return len;
-}
-
-int Train::getOpCount() {
-    return countOp;
-}
+#endif  // INCLUDE_TRAIN_H_
